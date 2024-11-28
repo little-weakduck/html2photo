7fc92e116e05ab6045bbbe7f27af30c05288b0fc
weakduckluo <weakduckluo@tencent.com>
2024-11-28 14:02:37 +0800

    host

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ if __name__ == '__main__':
         os.makedirs("images")
     if not os.path.exists("html"):
         os.makedirs("html")
-    app.run(debug=True,port=4455)
+    app.run(host='0.0.0.0',debug=True,port=4455)
