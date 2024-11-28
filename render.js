1baf21943545b388d92aee7ca022ae0c5bd76d05
weakduckluo <weakduckluo@tencent.com>
2024-11-28 18:59:22 +0800

    feat

--- render.js
+++ render.js
@@ -3,7 +3,6 @@ var system = require('system');
 var url = system.args[1];
 var output = system.args[2];
 
-page.viewportSize = { width: 1000}; // 设置视口大小
 page.settings.dpi = 144; // 设置 DPI 为 144
 
 page.open(url, function () {
