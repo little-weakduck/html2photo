807db27965d3eebcf080eef893ea31358d043d90
weakduckluo <weakduckluo@tencent.com>
2024-11-28 18:53:56 +0800

    kuandu

--- render.js
+++ render.js
@@ -3,6 +3,9 @@ var system = require('system');
 var url = system.args[1];
 var output = system.args[2];
 
+page.viewportSize = { width: 1000}; // 设置视口大小
+page.settings.dpi = 144; // 设置 DPI 为 144
+
 page.open(url, function () {
     page.render(output);
     phantom.exit();
